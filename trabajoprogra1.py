5348536c813e62f38fc1eeeca94fa4508bd1e754
donshi1 <24040003@utculiacan.edu.mx>
2025-08-25 14:26:54 -0700

    Add files via upload

---
+++ trabajoprogra1.py
@@ -0,0 +1,8 @@
+print ("hola mundo, como estan?")
+print("""1.bien\n2.mal""")
+resp= input("que escoges: " )
+if resp=="1":
+    print ("excelente, que tengas un buen dia.")
+elif resp=="2":
+    print("eso es malo, desahogate escribiendo lo que sientes: ")
+    input("yo me siento: ")
